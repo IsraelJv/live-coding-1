26391076af542c1e9668df6c0e9a3d7c96556542
Israel Jv <hectorjv97@gmail.com>
2024-02-08 17:28:33 -0600

    Se agrega calificacion

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // Escribe tu código aquí.
+const bigWords = (referenceString, arrayOfStrings) => {
+    return arrayOfStrings.filter(word => word.length > referenceString.length);
+}
+
+const printArray = (stringsArray) => {
+    const list = document.getElementById('wordsList'); 
+    list.innerHTML = ''; 
+    stringsArray.forEach(word => {
+        const listItem = document.createElement('li');
+        listItem.textContent = word;
+        list.appendChild(listItem);
+    });
+}
